5e81ee3e4c1cc4fc683bf30d2ffd1a99fca1011f
sai <saikrishna321@yahoo.com>
2013-05-21 14:50:06 +0530

    first commit

---
+++ src/test/java/com/test/example/YellowBookTest.java
@@ -0,0 +1,34 @@
+package com.test.example;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class YellowBookTest {
+
+	/**
+	 * @param args
+	 * @throws InterruptedException 
+	 */
+	@Test
+	public  void testYell() throws InterruptedException {
+		// TODO Auto-generated method stub
+
+		WebDriver driver = new FirefoxDriver();
+		driver.get("https://c.cs6.visual.force.com/apex/ScheduleSendDailyStrategicUpdate");
+		//driver.getTitle();
+       Thread.sleep(10000);
+		driver.findElement(By.xpath(".//*[@id='username']")).sendKeys("srinivasan.velusamy@photoninfotech.net.photon");
+		driver.findElement(By.xpath(".//*[@id='password']")).sendKeys("Me@ph0t0n");
+		driver.findElement(By.xpath(".//*[@id='Login']")).click();
+		new WebDriverWait(driver,40).until(ExpectedConditions.elementToBeClickable(By.xpath(".//*[@id='home_Tab']/a")));
+		driver.quit();
+	
+	}
+
+}
